65023d2f9ac8bde75c9320ede2c78460cfe37f3b
David Engel <david@airshipdevelopment.com>
2020-11-15 18:25:28 -0800

    Control tweaks

--- VideoRental/src/main/java/shop/main/Control.java
+++ VideoRental/src/main/java/shop/main/Control.java
@@ -23,6 +23,7 @@ class Control {
   private State _state;
 
   private UIFormMenu _getVideoForm;
+  private UIFormTest _yearTest;
   private UIFormTest _numberTest;
   private UIFormTest _stringTest;
     
@@ -35,23 +36,6 @@ class Control {
 	  ExitedState
   }
   
-  enum Entry {
-	  DEFAULT,
-	  ADD,
-	  CheckIn,
-	  CheckOut,
-	  PRINT,
-	  CLEAR,
-	  UNDO,
-	  REDO,
-	  TOP,
-	  ExitEntry,
-	  INIT,
-	  YES,
-	  NO
-  }
-  
-  
   Control(Inventory inventory, UI ui) {
     _inventory = inventory;
     _ui = ui;
@@ -61,7 +45,7 @@ class Control {
     addSTART(START);
     addEXIT(EXIT);
     
-    UIFormTest yearTest = new UIFormTest() {
+    _yearTest = new UIFormTest() {
         public boolean run(String input) {
           try {
             int i = Integer.parseInt(input);
@@ -70,7 +54,8 @@ class Control {
             return false;
           }
         }
-      };
+    };
+
     _numberTest = new UIFormTest() {
         public boolean run(String input) {
           try {
@@ -80,16 +65,17 @@ class Control {
             return false;
           }
         }
-      };
+    };
+
     _stringTest = new UIFormTest() {
-        public boolean run(String input) {
-          return ! "".equals(input.trim());
-        }
-      };
+		public boolean run(String input) {
+		  return ! "".equals(input.trim());
+		}
+    };
 
     UIFormMenuBuilderFactory f = new UIFormMenuBuilderFactory();
     f.add("Title", _stringTest);
-    f.add("Year", yearTest);
+    f.add("Year", _yearTest);
     f.add("Director", _stringTest);
     _getVideoForm = f.toUIFormMenu("Enter Video");
   }
@@ -97,207 +83,185 @@ class Control {
   void run() {
     try {
       while (_state != State.ExitedState) {
-    	  switch(_state) {
-    	  	case StartState:
-    	  		_ui.processMenu(_menus[START]);
-    	  		break;
-    	  	case ExitState:
-    	  		_ui.processMenu(_menus[EXIT]);
-    	  		break;
-    	  }
+	  switch(_state) {
+		case StartState:
+			_ui.processMenu(_menus[START]);
+			break;
+		case ExitState:
+			_ui.processMenu(_menus[EXIT]);
+			break;
+	  }
       }
     } catch (UIError e) {
       _ui.displayError("UI closed");
     }
   }
-  
-  
-  private UIMenuAction setAction(Entry e) {
-	  UIMenuAction a = null;
-	  
-	  switch (e) {
-	  	case DEFAULT:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          _ui.displayError("doh!");
-	  	        }
-	  		};
-	  	    break;
-	  	case ADD:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          String[] result1 = _ui.processForm(_getVideoForm);
-	  	          Video v = Data.newVideo(result1[0], Integer.parseInt(result1[1]), result1[2]);
 
-	  	          UIFormMenuBuilderFactory f = new UIFormMenuBuilderFactory();
-	  	          f.add("Number of copies to add/remove", _numberTest);
-	  	          String[] result2 = _ui.processForm(f.toUIFormMenu(""));
-	  	                                             
-	  	          Command c = Data.newAddCmd(_inventory, v, Integer.parseInt(result2[0]));
-	  	          if (! c.run()) {
-	  	            _ui.displayError("Command failed");
-	  	          }
-	  	        }
-	  		};
-	  	      break; 
-	  	case CheckIn:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          // TODO  
-	  	        	String[] result1 = _ui.processForm(_getVideoForm);
-	  	        	Video v = Data.newVideo(result1[0], Integer.parseInt(result1[1]), result1[2]);
-	  	        	
-	  	        	Command c = Data.newInCmd(_inventory, v);
-	  	        	if (! c.run()) {
-	  	                _ui.displayError("Command failed");
-	  	            }
-	  	        }
-	  		};
-	  		break;
-	  	case CheckOut:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          // TODO 
-	  	        	String[] result1 = _ui.processForm(_getVideoForm);
-	  	        	Video v = Data.newVideo(result1[0], Integer.parseInt(result1[1]), result1[2]);
-	  	        	
-	  	        	Command c = Data.newOutCmd(_inventory, v);
-	  	        	if (! c.run()) {
-	  	                _ui.displayError("Command failed");
-	  	            }
-	  	        }
-	  		};
-	  		break;
-	  	case PRINT:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          _ui.displayMessage(_inventory.toString());
-	  	        }
-	  		};
-	  		break;
-	  	case CLEAR:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          if (!Data.newClearCmd(_inventory).run()) {
-	  	            _ui.displayError("Command failed");
-	  	          }
-	  	        }
-	  		};
-	  		break;
-	  	case UNDO:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          if (!Data.newUndoCmd(_inventory).run()) {
-	  	            _ui.displayError("Command failed");
-	  	          }
-	  	        }
-	  		};
-	  		break;
-	  	case REDO:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          if (!Data.newRedoCmd(_inventory).run()) {
-	  	            _ui.displayError("Command failed");
-	  	          }
-	  	        }
-	  		};
-	  		break;
-	  	case TOP:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          // TODO  
-	  	        	Iterator<Record> itr = _inventory.iterator((r1,r2) -> r2.numRentals() - r1.numRentals());
-	  	        	int count = 0;
-	  	        	StringBuilder s = new StringBuilder();
-	  	        	while (itr.hasNext() && count < 10) {
-	  	        		Record r = itr.next();
-	  	        		s.append(r.toString()+ "\n");
-	  	        		count++;
-	  	        	}
-	  	        	_ui.displayMessage(s.toString());
-	  	        }
-	  		};
-	  		break;
-	  	case ExitEntry:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          _state = State.ExitState;
-	  	        }
-	  		};
-	  		break;  		
-	  	case INIT:
-	  		a = new UIMenuAction() {
-	  	        public void run() {
-	  	          Data.newAddCmd(_inventory, Data.newVideo("a", 2000, "m"), 1).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("b", 2000, "m"), 2).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("c", 2000, "m"), 3).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("d", 2000, "m"), 4).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("e", 2000, "m"), 5).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("f", 2000, "m"), 6).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("g", 2000, "m"), 7).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("h", 2000, "m"), 8).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("i", 2000, "m"), 9).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("j", 2000, "m"), 10).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("k", 2000, "m"), 11).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("l", 2000, "m"), 12).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("m", 2000, "m"), 13).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("n", 2000, "m"), 14).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("o", 2000, "m"), 15).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("p", 2000, "m"), 16).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("q", 2000, "m"), 17).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("r", 2000, "m"), 18).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("s", 2000, "m"), 19).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("t", 2000, "m"), 20).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("u", 2000, "m"), 21).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("v", 2000, "m"), 22).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("w", 2000, "m"), 23).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("x", 2000, "m"), 24).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("y", 2000, "m"), 25).run();
-	  	          Data.newAddCmd(_inventory, Data.newVideo("z", 2000, "m"), 26).run();
-	  	        }
-	  		};
-	  		break;
-	  	
-	  	case YES:
-	  		a = new UIMenuAction() {
-	        	public void run() {
-	        		_state = State.ExitedState;
-	        	}
-	  		};
-	  		break;
-	  	
-	  	case NO:
-	  		a = new UIMenuAction() {
-	        	public void run() {
-	        		_state = State.StartState;
-	        	}
-	  		};
-	  		break;
-	  }
-	  return a;
+  private UIMenuAction defaultAction() {
+	return new UIMenuAction() {
+		public void run() {
+			_ui.displayError("doh!");
+		}
+	};
+  }
+
+  private UIMenuAction addAction() {
+	return new UIMenuAction() {
+		public void run() {
+			String[] result1 = _ui.processForm(_getVideoForm);
+			Video v = Data.newVideo(result1[0], Integer.parseInt(result1[1]), result1[2]);
+
+			UIFormMenuBuilderFactory f = new UIFormMenuBuilderFactory();
+			f.add("Number of copies to add/remove", _numberTest);
+			String[] result2 = _ui.processForm(f.toUIFormMenu(""));
+
+			Command c = Data.newAddCmd(_inventory, v, Integer.parseInt(result2[0]));
+			if (! c.run()) {
+				_ui.displayError("Command failed");
+			}
+		}
+	};
+  }
+
+  private UIMenuAction checkInAction() {
+	return new UIMenuAction() {
+		public void run() {
+			String[] result1 = _ui.processForm(_getVideoForm);
+			Video v = Data.newVideo(result1[0], Integer.parseInt(result1[1]), result1[2]);
+
+			Command c = Data.newInCmd(_inventory, v);
+			if (! c.run()) {
+				_ui.displayError("Command failed");
+			}
+		}
+	};
+  }
+
+  private UIMenuAction checkOutAction() {
+	return new UIMenuAction() {
+		public void run() {
+			String[] result1 = _ui.processForm(_getVideoForm);
+			Video v = Data.newVideo(result1[0], Integer.parseInt(result1[1]), result1[2]);
+
+			Command c = Data.newOutCmd(_inventory, v);
+			if (! c.run()) {
+				_ui.displayError("Command failed");
+			}
+		}
+	};
+  }
+
+  private UIMenuAction printAction() {
+	return new UIMenuAction() {
+		public void run() {
+			_ui.displayMessage(_inventory.toString());
+		}
+	};
+  }
+
+  private UIMenuAction clearAction() {
+	return new UIMenuAction() {
+		public void run() {
+			if (!Data.newClearCmd(_inventory).run()) {
+				_ui.displayError("Command failed");
+			}
+		}
+	};
+  }
+
+  private UIMenuAction undoAction() {
+	return new UIMenuAction() {
+		public void run() {
+			if (!Data.newUndoCmd(_inventory).run()) {
+				_ui.displayError("Command failed");
+			}
+		}
+	};
+  }
+
+  private UIMenuAction redoAction() {
+	return new UIMenuAction() {
+		public void run() {
+			if (!Data.newRedoCmd(_inventory).run()) {
+				_ui.displayError("Command failed");
+			}
+		}
+	};
+  }
+
+  private UIMenuAction topAction() {
+	return new UIMenuAction() {
+		public void run() {
+			Iterator<Record> itr = _inventory.iterator((r1,r2) -> r2.numRentals() - r1.numRentals());
+			int count = 0;
+			StringBuilder s = new StringBuilder();
+			while (itr.hasNext() && count < 10) {
+				Record r = itr.next();
+				s.append(r.toString()+ "\n");
+				count++;
+			}
+			_ui.displayMessage(s.toString());
+		}
+	};
+  }
+
+  private UIMenuAction exitEntryAction() {
+	return new UIMenuAction() {
+		public void run() {
+			_state = State.ExitState;
+		}
+	};
+  }
+
+  private UIMenuAction initAction() {
+	return new UIMenuAction() {
+		public void run() {
+			char letter = 'a';
+			for (int i = 0; i < 26; i++) {
+				Data.newAddCmd(_inventory, Data.newVideo(String.valueOf(letter), 2000, "m"), i + 1).run();
+				letter++;
+			}
+		}
+	};
+  }
+
+  private UIMenuAction yesAction() {
+	return new UIMenuAction() {
+		public void run() {
+			_state = State.ExitedState;
+		}
+	};
+  }
+
+  private UIMenuAction noAction() {
+	return new UIMenuAction() {
+		public void run() {
+			_state = State.StartState;
+		}
+	};
   }
   
   private void addSTART(int stateNum) {
 	  UIFormMenuBuilderFactory m = new UIFormMenuBuilderFactory();
-	  m.add("Default",setAction(Entry.DEFAULT));
-	  m.add("Add/Remove copies of a video",setAction(Entry.ADD));
-	  m.add("Check in a video",setAction(Entry.CheckIn));
-	  m.add("Check out a video",setAction(Entry.CheckOut));
-	  m.add("Print the inventory",setAction(Entry.PRINT));
-	  m.add("Clear the inventory",setAction(Entry.CLEAR));
-	  m.add("Undo",setAction(Entry.UNDO));
-	  m.add("Redo",setAction(Entry.REDO));
-	  m.add("Print top ten all time rentals in order",setAction(Entry.TOP));    
-	  m.add("Exit",setAction(Entry.ExitEntry));
-	  m.add("Initialize with bogus contents",setAction(Entry.INIT));
+	  m.add("Default", defaultAction());
+	  m.add("Add/Remove copies of a video", addAction());
+	  m.add("Check in a video", checkInAction());
+	  m.add("Check out a video", checkOutAction());
+	  m.add("Print the inventory", printAction());
+	  m.add("Clear the inventory", clearAction());
+	  m.add("Undo", undoAction());
+	  m.add("Redo", redoAction());
+	  m.add("Print top ten all time rentals in order", topAction());
+	  m.add("Exit", exitEntryAction());
+	  m.add("Initialize with bogus contents", initAction());
 	  _menus[stateNum] = m.toUIFormMenu("Bob's Video");
   }
   
   private void addEXIT(int stateNum) {
 	  UIFormMenuBuilderFactory m = new UIFormMenuBuilderFactory();
-	  m.add("Default",setAction(Entry.DEFAULT));
-	  m.add("Yes",setAction(Entry.YES));
-	  m.add("No",setAction(Entry.NO));
+	  m.add("Default", defaultAction());
+	  m.add("Yes", yesAction());
+	  m.add("No", noAction());
 	  _menus[stateNum] = m.toUIFormMenu("Are you sure you want to exit?");
   }
 }
